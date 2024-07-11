c7b47c05a5035b7c3d5a7e5c743ecbb5e4e5f0bd
Ole Backhaus <ole.backhaus@gmx.de>
2024-07-11 10:05:44 +0200

    Add show-answer functionality

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // phew… not a lot going on here. Please add some code!
 console.clear();
 
+// Toggle Bookmark
+
 const bmToggle = document.querySelector('[data-js="bookmark"]');
 
 bmToggle.addEventListener("click", () => {
   bmToggle.classList.toggle("bookmark--active");
   console.log("toggled between dark-/light");
+});
+
+// Make Answer visible / invisible
+
+const button = document.querySelector('[data-js="button"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+button.addEventListener("click", () => {
+  answer.classList.toggle("hidden");
+  console.log("toggled between answer is visible/invisible");
+});
+
+// Change Text on active button
+
+button.addEventListener("click", () => {
+  button.textContent("hidden");
+  console.log("Changed Text on active button");
 });
